e4459b408fcb2e74fcf19947740dff4948d09520
Panos Matsinopoulos <panos@mixlr.com>
2023-06-07 13:10:57 +0300

    Added a button that displays a Toast inside the Dashboard Fragment

--- app/src/main/java/com/mixlr/panos/fragmentsdemo/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/mixlr/panos/fragmentsdemo/ui/dashboard/DashboardFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.mixlr.panos.fragmentsdemo.databinding.FragmentDashboardBinding
@@ -31,6 +32,9 @@ class DashboardFragment : Fragment() {
         val textView: TextView = binding.textDashboard
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }
+        binding.btnTest.setOnClickListener {
+            Toast.makeText(activity, "Clicked", Toast.LENGTH_LONG).show()
         }
         return root
     }
